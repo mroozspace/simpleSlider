dbfb7e19e892979b09e688e8f82a4a2d42e25130
adrian mroz <adrian.mroz@besidethepark.com>
2019-02-10 22:11:01 +0100

    slider layout & logic done

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
+const transitionTime = 300
+
+const body = document.querySelector('body')
+const contacts = [...document.querySelectorAll('.card')]
+const closeModalBtn = document.querySelector('.modal__close')
+const modal = document.querySelector('.modal')
+
 const mySwiper = new Swiper ('.swiper-container', {
   direction: 'horizontal',
   loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -11,3 +15,29 @@ const mySwiper = new Swiper ('.swiper-container', {
   spaceBetween: 10,
   centerSlides: true
 })
+
+
+closeModalBtn.addEventListener('click', () => {
+  body.classList.remove('scroll-lock')
+  modal.classList.add('modal--hiding')
+  setTimeout(() => {
+    modal.classList.remove('modal--hiding')
+    modal.classList.replace('modal--visible' ,'modal--hidden')
+  }, transitionTime);
+})
+
+const slideTo = id => {
+  body.classList.add('scroll-lock')
+  modal.classList.replace('modal--hidden' ,'modal--revealing')
+  mySwiper.slideTo(id)
+  setTimeout(() => modal.classList.replace('modal--revealing', 'modal--visible'), transitionTime);
+}
+
+contacts.forEach(
+  contact => contact.addEventListener('click',
+  ({target}) => {
+      if (target.classList.value != 'card__envelope') {
+        slideTo(contact.id)
+      }
+    }
+));
